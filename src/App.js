faba30377ffc83da86e81a0e08f7b640bdf7c596
Anastasiia Dashko <anastasiia.dashko@student.tuke.sk>
2022-01-08 19:11:42 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react";
+import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (<div>
+        <Footer />
+            <Header />
+        </div>
+    );
 }
-
+const Techno = () =>{
+    return (
+        <div >
+            <ul>
+                <li>k</li>
+                <li>f</li>
+            </ul>
+        </div>
+    );
+}
+const Header = () => {
+    return (
+        <div >
+                <a href='#'>m</a>
+                <a href='#'>l</a>
+        </div>
+    );
+}
 export default App;
