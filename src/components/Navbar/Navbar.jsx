b261871fed86c3a77f5f93c73d65b2730c6ec671
Anastasiia Dashko <anastasiia.dashko@student.tuke.sk>
2022-03-20 16:58:51 +0100

    users.... урок 55

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,23 +6,24 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+                <NavLink to="/profile" className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to={"/dialogs"} className= { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
+                <NavLink to={"/dialogs"} className={navData => navData.isActive ? s.active : s.item}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to={"/news"}className= { navData => navData.isActive ? s.active : s.item }>News</NavLink>
+                <NavLink to={"/news"} className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to={"/music"}className= { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
-            </div >
-            <div className={s.item}>
-                <NavLink to={"/settings"}className= { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
+                <NavLink to={"/music"} className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to={"/friends"}className= { navData => navData.isActive ? s.active : s.item }>Friends</NavLink>
+                <NavLink to={"/settings"} className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
             </div>
+
         </nav>
     );
 }
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,27 +1,35 @@
 import React from "react";
-import s from './Users.module.css';
 import {connect} from "react-redux";
+import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer";
 import Users from "./Users";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
-    }
-}
-let mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followAC(userId))
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowAC(userId))
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users))
-        }
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
+let mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId));
+        },
+        unfollow: (userId) => {
+            dispatch(unfollowAC(userId));
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setUsersTotalCountAC(totalCount))
+        }
+    }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
