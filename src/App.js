1a1d52d3125f365d7ea0c9aff9382ed4e22325c8
Anastasiia Dashko <anastasiia.dashko@student.tuke.sk>
2022-03-22 12:21:22 +0100

    Auth me login
     61

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import {Route, Routes} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = () => {
     return (
         <div className='appWrapper'>
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className='appWrapperContent'>
                 <Routes>
                     <Route path='/dialogs' element={<DialogsContainer/>}/>
-                    <Route path='/profile' element={<ProfileContainer/>}/>
+                    <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
                     <Route path='/users' element={<UsersContainer/>}/>
                 </Routes>
             </div>
